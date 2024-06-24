a8c33a7a31b6c1dd8b603f63daca761d7068f804
Davit Tavadze <davit@techpheonix.dev>
2024-06-24 20:00:54 +0400

    Connection to the database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Database is connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 const app = express()
 
 app.listen(8080, () => {
-  console.log('Server is running on port :8080')
+  console.log('Server is running on port 8080')
 })
